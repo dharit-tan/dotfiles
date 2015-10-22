2bbb8166468d6c0f4815a9620c5abefce53f4dc2
Dharit Tantiviramanond <reedt@purestorage.com>
2015-10-22 13:58:44 -0700

    symlinks.bash

---
+++ symlinks.bash
@@ -0,0 +1,7 @@
+cd  
+ln -s dotfiles/.spacemacs .spacemacs
+ln -s dotfiles/.bashrc .bashrc
+ln -s dotfiles/.git-commit-template.txt .git-commit-template.txt
+ln -s dotfiles/.gitconfig .gitconfig
+ln -s dotfiles/git-completion.bash git-completion.bash
+ln -s dotfiles/cscope_gen.sh cscope_gen.sh
